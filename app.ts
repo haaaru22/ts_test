ccdaa969c1b524d3eb77988801c318b795227f5b
haru <haru@harunoMacBook-Air.local>
2021-04-10 08:59:07 +0900

    Tsの型

--- app.ts
+++ app.ts
@@ -7,3 +7,11 @@ const sum = (s1:number,s2:number): number =>{
 }
 
 console.log(sum(3, 5.5));
+
+let hasValue: boolean = false;
+let count: number = 3;
+let float: number = 3.11;
+let negative: number = -5;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
