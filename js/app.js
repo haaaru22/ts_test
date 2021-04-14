347803377263abdaed614576c887dc9c9c0452ac
haru <haru@pop-os.localdomain>
2021-04-14 21:42:39 +0900

    Element

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const test = document.getElementById('test');
+//Element
+test.classList.add('red');
+
+const input = document.getElementById('ap');
+input.type = 'text';
+console.log(input);
+
+const cl = document.querySelectorAll('.cl');
+console.log(test);
+console.log(cl);
